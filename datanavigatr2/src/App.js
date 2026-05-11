1283e37b1f08c88802a3532ce5d2758589f0b2bd
Emmett Smyth <ewyattsmyth@gmail.com>
2026-05-11 12:21:25 +0800

    Bug fiix

--- datanavigatr2/src/App.js
+++ datanavigatr2/src/App.js
@@ -2,7 +2,7 @@ import AppRouter from "./app/router/AppRouter";
 
 /*
  * Create React App's default App entry now delegates to the real router in
- * src/app/router/AppRouter.jsx.
+ * src/app/router/AppRouter.jsx
  */
 function App() {
   return <AppRouter />;
